4cc123dfb7dcbb2906f673c649740d7114fc52ef
Ewerton Filipe <xewertonfilipe@gmail.com>
2019-02-21 15:52:46 -0300

    concluído refatoração 1.0

--- challenge-29/challenge-29.js
+++ challenge-29/challenge-29.js
@@ -16,22 +16,17 @@
     - Placa
     - Cor
     - e um botão "Cadastrar"
-
   Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
   carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
   aparecer no final da tabela.
-
   Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
   empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
   Dê um nome para a empresa e um telefone fictício, preechendo essas informações
   no arquivo company.json que já está criado.
-
   Essas informações devem ser adicionadas no HTML via Ajax.
-
   Parte técnica:
   Separe o nosso módulo de DOM criado nas últimas aulas em
   um arquivo DOM.js.
-
   E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
   que será nomeado de "app".
   */
@@ -39,11 +34,7 @@
   function app() {
     var fragment = doc.createDocumentFragment();
     var $form = new DOM('[data-js="form"]');
-    var $urlImagem = new DOM('[data-js="url-imagem"]');
-    var $marca = new DOM('[data-js="marca"]');
-    var $ano = new DOM('[data-js="ano"]');
-    var $placa = new DOM('[data-js="placa"]');
-    var $cor = new DOM('[data-js="cor"]');
+    
     var $table = new DOM('[data-js="table"]');
 
     function initialize() {
@@ -51,57 +42,62 @@
     }
     
     function submitForm() {
-      $form.on('submit', addCarInTable);
+      $form.on('submit', createTd);
     }
 
-    function addCarInTable(event) {
+    function createTd(event) {
       event.preventDefault();
-      var data = capturesInputValue();
-      var trCompleta = doc.createElement('tr');
-      var tdUrl = doc.createElement('td');
-      var tdMarca = doc.createElement('td');
-      var tdAno = doc.createElement('td');
-      var tdPlaca = doc.createElement('td');
-      var tdCor = doc.createElement('td');
-      var url = doc.createTextNode(data.url);
-      var marca = doc.createTextNode(data.marca);
-      var ano = doc.createTextNode(data.ano);
-      var placa = doc.createTextNode(data.placa);
-      var cor = doc.createTextNode(data.cor);
-      tdUrl.appendChild(url);
-      tdMarca.appendChild(marca);
-      tdAno.appendChild(ano);
-      tdPlaca.appendChild(placa);
-      tdCor.appendChild(cor);
-      trCompleta.appendChild(tdUrl);
-      trCompleta.appendChild(tdMarca);
-      trCompleta.appendChild(tdAno);
-      trCompleta.appendChild(tdPlaca);
-      trCompleta.appendChild(tdCor);
-      createElementTd(trCompleta);
+      var arrayTds = [];
+      var totalTd = totalThElement();
+      for(let index = 0; index < totalTd; index++) {
+        arrayTds.push(doc.createElement('td'));
+      }
+      return addNodeInTd(arrayTds);
     }
 
-    function hasChildInTable() {
-      return $table.get()[0].firstElementChild;
+    function addNodeInTd(arrayTds) {
+      var textsNodes = capturesInputValue();
+      arrayTds.forEach(function(td, index) {
+        td.appendChild(textsNodes[index]);
+      });
+      return addTdInTr(arrayTds);
     }
 
-    function createElementTd(element) {
-      return $table.get()[0].appendChild(element);
+    function addTdInTr(tds) {
+      var tr = doc.createElement('tr');
+      tds.forEach(function(item) {
+        tr.appendChild(item);
+      });
+      return addTrFullInTable(tr);
+    }
+
+    function addTrFullInTable(tr) {
+      $table.get()[0].appendChild(tr);
+    }
+
+    function totalThElement() {
+      return $table.get()[0].firstElementChild.firstElementChild
+        .childElementCount;
     }
 
     function capturesInputValue() {
-      var url = doc.createTextNode($urlImagem.get()[0].value);
-      var marca = doc.createTextNode($marca.get()[0].value);
-      var ano = doc.createTextNode($ano.get()[0].value);
-      var placa = doc.createTextNode($placa.get()[0].value);
-      var cor = doc.createTextNode($cor.get()[0].value);
-      return {
-        url: url.textContent,
-        marca: marca.textContent,
-        ano: ano.textContent,
-        placa: placa.textContent,
-        cor: cor.textContent
-      };
+      var $urlImagem = new DOM('[data-js="url-imagem"]');
+      var $marca = new DOM('[data-js="marca"]');
+      var $ano = new DOM('[data-js="ano"]');
+      var $placa = new DOM('[data-js="placa"]');
+      var $cor = new DOM('[data-js="cor"]');
+      var url = $urlImagem.get()[0].value;
+      var marca = $marca.get()[0].value;
+      var ano = $ano.get()[0].value;
+      var placa = $placa.get()[0].value;
+      var cor = $cor.get()[0].value;
+      return [
+        doc.createTextNode(url),
+        doc.createTextNode(marca),
+        doc.createTextNode(ano),
+        doc.createTextNode(placa),
+        doc.createTextNode(cor)
+      ];
     }
 
     return {
